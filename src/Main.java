4fc208741f8beddbc5d7a0ff698f99963e961601
Dima.Voron13 <dima.voron13666@gmail.com>
2024-06-11 23:29:25 +0300

    коммит 3

--- src/Main.java
+++ src/Main.java
@@ -3,51 +3,51 @@ public class Main {
 
         System.out.println("Задание 1");
 
-        for (byte i = 1; i <= 10; i ++) {
-            System.out.println(i);
+        for (byte impaledStacks = 1; impaledStacks <= 10; impaledStacks++) {
+            System.out.println(impaledStacks);
         }
 
         System.out.println("Задание 2");
 
-        for (byte q = 10; q >= 1; q --) {
-            System.out.println(q);
+        for (byte evenNumber = 10; evenNumber >= 1; evenNumber--) {
+            System.out.println(evenNumber);
         }
 
         System.out.println("Задание 3");
 
-        for (int w = 0; w < 17; w = w + 2) {
-            System.out.println(w);
+        for (int countDown = 0; countDown < 17; countDown = countDown + 2) {
+            System.out.println(countDown);
         }
 
         System.out.println("Задание 4");
 
-        for (int e = 10; e >= -10; e --) {
-            System.out.println(e);
+        for (int countReverse = 10; countReverse >= -10; countReverse--) {
+            System.out.println(countReverse);
         }
 
         System.out.println("Задание 5");
 
-        for (int r = 1904; r <= 2096; r = r + 4) {
-            System.out.println(r + " год являяется високосным");
+        for (int yearLeap = 1904; yearLeap <= 2096; yearLeap = yearLeap + 4) {
+            System.out.println(yearLeap + " год являяется високосным");
         }
 
         System.out.println("Задание 6");
 
-        for (int t = 7; t <= 98; t = t + 7) {
-            System.out.println(t);
+        for (int timeRunning = 7; timeRunning <= 98; timeRunning = timeRunning + 7) {
+            System.out.println(timeRunning);
         }
 
         System.out.println("Задание 7");
 
-        for (int y = 1; y <= 512; y = y * 2) {
-            System.out.println(y);
+        for (int youngPeople = 1; youngPeople <= 512; youngPeople = youngPeople * 2) {
+            System.out.println(youngPeople);
         }
 
         System.out.println("Задание 8");
 
         int money = 29000;
         int total = 0;
-        for (int y = 0; y <= 12; y ++) {
+        for (int y = 0; y <= 12; y++) {
             total = total + money;
             System.out.println("Месяц " + y + ", сумма накоплений равна " + total + " рублей");
         }
@@ -56,16 +56,16 @@ public class Main {
 
         int money1 = 29000;
         int total1 = 0;
-        for (int u = 0; u <= 12; u ++) {
-            total1 = total1 + total1/100;
+        for (int u = 0; u <= 12; u++) {
+            total1 = total1 + total1 / 100;
             total1 = total1 + money1;
             System.out.println("Месяц " + u + ", сумма накоплений равна " + total1 + " рублей");
         }
 
         System.out.println("Задание 10");
-        byte a = 2;
-        for (int p = 1; p <= 10; p ++) {
-            System.out.println(a + " * " + p + " = " + p * a);
+        byte multiplier = 2;
+        for (int p = 1; p <= 10; p++) {
+            System.out.println(multiplier + " * " + p + " = " + p * multiplier);
         }
     }
 }
